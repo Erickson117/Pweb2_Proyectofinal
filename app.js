d7ebaf303962876b326f44b7942205437658ab95
Erickson117 <erickreyes07@aragon.unam.mx>
2022-11-28 15:47:35 -0600

    Agregar menu del proyecto final sin las tareas aun

---
+++ app.js
@@ -0,0 +1,30 @@
+
+//const jugadores= ["Messi","Cristiano","Muller","Cafu"];
+//console.log("longitud: "+jugadores.length);
+//console.log("Valor indice 2: "+jugadores[2]);
+
+//var condicion = true;
+//console.log(condicion ? true : false);
+
+/*arreglo = [1,2,3];
+for(var i = 0; i < arreglo.length; i++){
+  var numeroVar = arreglo[i];
+  //let numeroLet = arreglo[i];
+  const numeroConst = arreglo[i];
+}
+
+// O aquí
+console.log("var numero", numeroVar);
+//console.log("let numero", numeroLet);
+console.log("const numero", numeroConst);
+*/
+
+var num1 = 0;
+num1 = 1;
+console.log(num1);
+let num2 = 0;
+num2 = 1;
+console.log(num2);
+const num3 = 0;
+num3++;
+console.log(num3);
